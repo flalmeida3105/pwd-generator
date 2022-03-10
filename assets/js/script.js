39d4ac09d1fafed3d4938c7f87c6e4abbe63cb6a
Fernando Almeida <nando.lalmeida@gmail.com>
2022-03-09 23:08:17 -0500

    added logic for only number and only special characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,49 +16,57 @@ var includeLetter = function (n) {
   var letters = window.confirm("Do you want to include upper case and lower case letters?");
   var numbers = window.confirm("Do you want to include numbers?");
   var specialCharacters = window.confirm("Do you want to include special characters?");
-    if(letters == true && numbers == true && specialCharacters == true) {
-      // var randomCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-      var randomCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    // Only LETTERS = TRUE
+    if (letters == true && numbers == false && specialCharacters == false) {
+      var randomCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
       var results = "";
       for (var i = 0; i < n; i++) {
-        // results += letters.charAt(Math.floor(Math.random() * letters.length));
         results += randomCharacters.charAt(Math.floor(Math.random() * randomCharacters.length));
-        // console.log(results);
-      }
-    } else if (letters == true && numbers == false && specialCharacters == false) {
-      var randomCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        var results = "";
-        for (var i = 0; i < n; i++) {
-          results += randomCharacters.charAt(Math.floor(Math.random() * randomCharacters.length));
-          // results += letters.charAt(Math.floor(Math.random() * letters.length) + (Math.floor(Math.random() * numbers.length)));
-          // console.log(results);
-        }
-    } else if (letters == true && numbers == true && specialCharacters == false) {
+      } 
+    } // LETTERS and NUMBERS = TRUE
+      else if (letters == true && numbers == true && specialCharacters == false) {
       var randomCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
       var results = "";
       for (var i = 0; i < n; i++) {
-        // results += letters.charAt(Math.floor(Math.random() * letters.length));
         results += randomCharacters.charAt(Math.floor(Math.random() * randomCharacters.length));
-        // console.log(results);
       }
-    } else if (letters == true && numbers == false && specialCharacters == true) {
+    } // LETTERS, NUMBERS and SPECIAL CHARACTERS = TRUE
+      else if(letters == true && numbers == true && specialCharacters == true) {
+      var randomCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+      var results = "";
+      for (var i = 0; i < n; i++) {
+        results += randomCharacters.charAt(Math.floor(Math.random() * randomCharacters.length));
+      }
+    } // LETTERS and SPECIAL CHARACTERS = TRUE
+      else if (letters == true && numbers == false && specialCharacters == true) {
       var randomCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
       var results = "";
       for (var i = 0; i < n; i++) {
-        // results += letters.charAt(Math.floor(Math.random() * letters.length));
         results += randomCharacters.charAt(Math.floor(Math.random() * randomCharacters.length));
-        // console.log(results);
       }
-    } else if (letters == false && numbers == true && specialCharacters == true) {
+    } // NUMBERS and SPECIAL CHARACTERS = TRUE
+      else if (letters == false && numbers == true && specialCharacters == true) {
       var randomCharacters = "012345689!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
       var results = "";
       for (var i = 0; i < n; i++) {
-        // results += letters.charAt(Math.floor(Math.random() * letters.length));
         results += randomCharacters.charAt(Math.floor(Math.random() * randomCharacters.length));
-        // console.log(results);
-      }
+      } 
+    } // Only SPECIAL CHARACTERS = TRUE
+      else if (letters == false && numbers == false && specialCharacters == true) {
+      var randomCharacters = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+      var results = "";
+      for (var i = 0; i < n; i++) {
+        results += randomCharacters.charAt(Math.floor(Math.random() * randomCharacters.length));
+      } 
+    } // Only NUMBERS = TRUE
+      else if (letters == false && numbers == true && specialCharacters == false) {
+      var randomCharacters = "012345689";
+      var results = "";
+      for (var i = 0; i < n; i++) {
+        results += randomCharacters.charAt(Math.floor(Math.random() * randomCharacters.length));
+      } 
     }
-  return results;
+    return results;
   // return letter;
 }
 
